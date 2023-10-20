4069af866afd6c7ffe2c4e548dbc44cc35c8c18c
500 <1094410019@qq.com>
2023-10-21 00:36:14 +0800

    0.1

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,7 +1,5 @@
 module.exports =
 {
-    editLinks: true,
-    lastUpdated: 'Last Updated', // string | boolean
     base: '/test/',
     locales:
     {
@@ -13,6 +11,21 @@ module.exports =
     },
     themeConfig:
     {
+        // 假定是 GitHub. 同时也可以是一个完整的 GitLab URL
+        repo: 'FH500/test',
+        // 自定义仓库链接文字。默认从 `themeConfig.repo` 中自动推断为
+        // "GitHub"/"GitLab"/"Bitbucket" 其中之一，或是 "Source"。
+        repoLabel: '查看源码',
+
+        // 以下为可选的编辑链接选项
+
+        // 假如文档不是放在仓库的根目录下：
+        docsDir: 'docs',
+        // 假如文档放在一个特定的分支下：
+        docsBranch: 'main',
+        editLinks: true,
+        editLinkText: '在 Github 上编辑此页',
+        lastUpdated: '最后更新时间',
         nav:
         [
             { text: "C/C++", link: "/" }
